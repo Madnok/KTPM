7f5bd1ce6cbcd3c38bef4e41e260eabc07d9a320
H31M44\Student <Student>
2024-03-08 16:47:56 +0700

    cài đặt pom.xml và app.properties

--- UserLogin/src/main/java/com/example/UserLogin/UserLoginApplication.java
+++ UserLogin/src/main/java/com/example/UserLogin/UserLoginApplication.java
@@ -8,6 +8,7 @@ public class UserLoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserLoginApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
