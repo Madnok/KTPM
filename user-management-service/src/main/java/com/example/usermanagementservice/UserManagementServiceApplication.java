7f124bdd725c4a06f28b2d281aed0bc944645110
Madnok <hungviper016@gmail.com>
2024-03-08 19:36:56 +0700

    .

--- user-management-service/src/main/java/com/example/usermanagementservice/UserManagementServiceApplication.java
+++ user-management-service/src/main/java/com/example/usermanagementservice/UserManagementServiceApplication.java
@@ -2,13 +2,12 @@ package com.example.usermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.usermanagementservice.model")
 public class UserManagementServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementServiceApplication.class, args);
-		System.out.println("hello world");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
 }
