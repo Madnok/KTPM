6bee67c08414aa0113c477c73ed4e53bb42a4967
Madnok <hungviper016@gmail.com>
2024-03-25 13:28:43 +0700

    add service order

---
+++ Week4/Services2_Oder/src/main/java/com/example/Services2_Oder/Services2OderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Services2_Oder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Services2OderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Services2OderApplication.class, args);
+	}
+
+}
---
+++ Week4/Services2_Oder/src/test/java/com/example/Services2_Oder/Services2OderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Services2_Oder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Services2OderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
